990082680bdd1d354ad5dc2ac0061493a938c9fc
STONE <17th@inbox.ru>
2024-12-05 14:51:34 +0300

    version_1_6_kill_mw

--- handlers/commands.py
+++ handlers/commands.py
@@ -1,3 +1,5 @@
+import datetime
+
 from aiogram import Bot, Router, F
 from aiogram.types import Message
 from aiogram.filters import Command, CommandObject
@@ -6,19 +8,19 @@ from aiogram.fsm.context import FSMContext
 from classes import Amount, Month, Day
 from data_base import DataBase
 from fsm.states import CallbackState
-from middlewares import DateMiddleware
 from keyboards import ikb_current_month, ikb_day_button
 
 command_router = Router()
 all_messages_router = Router()
 
-command_router.message.middleware(DateMiddleware())
-
 
 @command_router.message(Command('start'))
 @command_router.message(Command('start'), CallbackState())
-async def command_start(message: Message, state: FSMContext, current_year: int, current_month: int):
+async def command_start(message: Message, state: FSMContext):
     await state.clear()
+    current_date = datetime.datetime.now()
+    current_month = current_date.month
+    current_year = current_date.year
     msg = f'Приветствую, {message.from_user.full_name}!'
     user_tg_id = message.from_user.id
     if message.forward_origin:
@@ -65,52 +67,60 @@ async def count_tasks(message: Message, bot: Bot, command: CommandObject):
 
 
 @all_messages_router.message()
-async def go_to_date(message: Message, bot: Bot, current_year: int, current_month: int):
+async def go_to_date(message: Message, bot: Bot):
+    current_date = datetime.datetime.now()
+    current_month = current_date.month
+    current_year = current_date.year
     date = message.text.split()
     day, month, year = None, current_month, current_year
     await bot.delete_message(
         chat_id=message.from_user.id,
         message_id=message.message_id,
     )
+    ready = False
     if all(map(lambda x: x.isdigit(), date)):
         date = tuple(map(int, date))
         match date:
-            case [day]:
-                if 1 <= day <= Month(year, month).day_amount():
-                    day = day
-            case [day, month]:
-                if Amount.MIN_MONTH.value <= month <= Amount.MAX_MONTH.value:
-                    if 1 <= day <= Month(year, month).day_amount():
-                        day, month = day, month
-            case [day, month, year]:
-                if Amount.MIN_MONTH.value <= month <= Amount.MAX_MONTH.value:
-                    if 1 <= day <= Month(year, month).day_amount():
+            case [iday]:
+                if 1 <= iday <= Month(year, month).day_amount():
+                    day = iday
+                    ready = True
+            case [iday, imonth]:
+                if Amount.MIN_MONTH.value <= imonth <= Amount.MAX_MONTH.value:
+                    if 1 <= iday <= Month(year, imonth).day_amount():
+                        day, month = iday, imonth
+                        ready = True
+            case [iday, imonth, iyear]:
+                if Amount.MIN_MONTH.value <= imonth <= Amount.MAX_MONTH.value:
+                    if 1 <= iday <= Month(year, imonth).day_amount():
                         if len(str(year)) in (2, 4):
                             if len(str(year)):
                                 year += 2000
-                            day, month, year = day, month, year
-        if day is None:
-            msg = '\nВыбери день:'
-            await message.answer(
-                text=msg,
-                reply_markup=ikb_current_month(
-                    message.from_user.id,
-                    current_year,
-                    current_month,
+                            day, month, year = iday, imonth, iyear
+                            ready = True
+        if ready:
+            if day is None:
+                msg = '\nВыбери день:'
+                await message.answer(
+                    text=msg,
+                    reply_markup=ikb_current_month(
+                        message.from_user.id,
+                        current_year,
+                        current_month,
+                    )
                 )
-            )
-        else:
-            day = Day(
-                message.from_user.id,
-                year,
-                month,
-                day,
-            )
-            await bot.send_message(
-                **day.tasks_caption(),
-                chat_id=message.from_user.id,
-                reply_markup=ikb_day_button(
-                    target_day=day,
-                    admin=True,
-                ),
-            )
+            else:
+                day = Day(
+                    message.from_user.id,
+                    year,
+                    month,
+                    day,
+                )
+                await bot.send_message(
+                    **day.tasks_caption(),
+                    chat_id=message.from_user.id,
+                    reply_markup=ikb_day_button(
+                        target_day=day,
+                        admin=True,
+                    ),
+                )
--- middlewares/__init__.py
+++
@@ -1,8 +0,0 @@
-from .inner import DateMiddleware, MonthClass
-from .outer import OuterDateMiddleware
-
-__all__ = [
-    'DateMiddleware',
-    'MonthClass',
-    'OuterDateMiddleware',
-]
